b56aec00ba56454d7676a6d1ea64f2d1de1b3f84
rworr <rworr@uwaterloo.ca>
2017-06-05 15:48:38 -0400

    Compare similarity of current and next modules

--- index.py
+++ index.py
@@ -15,13 +15,10 @@ dim = 32
 isi = 1.0
 neurons_per_dim = 50
 cconv_neurons = 200
-sim_time = 4.0
+sim_time = 60.0
 
 vocab = spa.Vocabulary(dim)
 vocab.add('POS', SemanticPointer(dim).unitary())
-vocab.add('POS2', vocab.parse('POS * POS'))
-vocab.add('POS3', vocab.parse('POS2 * POS'))
-vocab.add('POS4', vocab.parse('POS3 * POS'))
 
 def clock(t):
     if (t % isi) < isi/2.0:
@@ -66,17 +63,32 @@ t = sim.trange()
 cur_data = sim.data[cur_probe]
 next_data = sim.data[next_probe]
 
-def plot_pointers(data, vocab, title='Similarity'):
+def normalized_dot(x, y):
+    xn = np.linalg.norm(x)
+    if xn != 0:
+        x = x / np.linalg.norm(x)
+    yn = np.linalg.norm(y)
+    if yn != 0:
+        y = y / np.linalg.norm(y)
+    return np.dot(x, y)
+
+def plot_similarity(similarity, title='Similarity'):
     ymin, ymax = -1.2, 1.2
     plt.figure()
     plt.ylim(ymin, ymax)
     plt.grid(True)
-    plt.plot(t, spa.similarity(data, vocab))
+    plt.plot(t, similarity)
     plt.title(title)
     plt.xlabel('Time')
     plt.xlim(right=t[-1])
     plt.legend(vocab.keys(), loc='upper center')
     plt.show()
 
-plot_pointers(cur_data, vocab, 'current')
-plot_pointers(next_data, vocab, 'next')
+def plot_pointers(data, vocab, title='Similarity'):
+    plot_similarity(spa.similarity(data, vocab), title)
+
+def plot_index_sim(cur, next):
+    data = np.asarray([normalized_dot(cur[i], next[i]) for i in range(0, len(cur))])
+    plot_similarity(data)
+
+plot_index_sim(cur_data, next_data)
